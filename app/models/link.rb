30559f9b8399d591b7a5ce2fdf1835269ab4bbbc
Aila Lu <ailalutw@gmail.com>
2022-10-03 17:51:05 +0800

    TDD for model Link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ApplicationRecord
+  validates_presence_of :lookup_code, :url
+  validates_uniqueness_of :lookup_code
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,13 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  it "always has a URL and always has a lookup code" do
+
+  it "is valid if always has a URL and always has a lookup code" do
+    link = Link.new(
+      url:  "https://www.youtube.com/watch?v=ZBs1a6Y25bY&list=PLjItgYqIzJ9WGy9WMf-44DXHB_7aWwJMc",
+      lookup_code: "1234567"
+    )
+    expect(link.valid?).to be(true)
+  end
+
+  it "is invalid if no url" do
+    link = Link.new(
+      lookup_code: "1234567"
+    )
+    expect(link.valid?).to be(false)
+  end
+
+  it "is invalid if no lookup code" do
+    link = Link.new(
+      url:  "https://www.youtube.com/watch?v=ZBs1a6Y25bY&list=PLjItgYqIzJ9WGy9WMf-44DXHB_7aWwJMc"
+    )
+    expect(link.valid?).to be(false)
+  end
+
+  it "is invalid if lookup code is not unique" do
     link = Link.new(
       url:  "https://www.youtube.com/watch?v=ZBs1a6Y25bY&list=PLjItgYqIzJ9WGy9WMf-44DXHB_7aWwJMc",
       lookup_code: "1234567"
     )
     link.save
-    
+    link2 = Link.new(
+      url:  "https://www.youtube.com/watch?v=ZBs1a6Y25bY&list=PLjItgYqIzJ9WGy9WMf-44DXHB_7aWabcd",
+      lookup_code: "1234567"
+    )
+    expect(link2.valid?).to be(false)
   end
 
 end
